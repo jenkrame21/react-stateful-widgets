e2a6d67f9c4bf325ccf0df5452f41ef9264d8dd3
Jennifer Kramer <jenkrame@gmail.com>
2020-10-05 17:18:58 -0400

    Completed Counter.js file. Working on Moods.js file next.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -53,14 +53,16 @@ export default function Counter() {
   const [count, setCount] = useState(0)
 
   const increment = () => {
-  /* STEP 4 */
-    setCount(count%2 + 1);
+    /* STEP 4 */
+    setCount(count + 1);
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count - 1);
   };
   const reset = () => {
     /* STEP 6 */
+    setCount(0);
   };
 
   const style = {
@@ -73,7 +75,7 @@ export default function Counter() {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number is {count%2 ? 'odd' : 'even' /* STEP 3 */}
+        Number {count} is {count%2 ? 'odd' : 'even' /* STEP 3 */}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
